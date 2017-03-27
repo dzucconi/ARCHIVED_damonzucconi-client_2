503ff51598be5aa13cc1226a6b30c31c48f4ec52
dzucconi <mail@damonzucconi.com>
2017-03-27 00:04:04 -0400

    Trim railties

--- config/application.rb
+++ config/application.rb
@@ -2,9 +2,6 @@ require_relative 'boot'
 
 require 'action_controller/railtie'
 require 'action_view/railtie'
-require 'active_job/railtie'
-require 'action_cable/engine'
-require 'rails/test_unit/railtie'
 require 'sprockets/railtie'
 
 Bundler.require(*Rails.groups)
