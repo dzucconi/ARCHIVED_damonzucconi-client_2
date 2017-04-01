6c0c1b78482eb6a72f7db614d66172fccfd60f6c
dzucconi <mail@damonzucconi.com>
2017-03-31 23:12:00 -0400

    Implements sidebar active centering

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,14 +1,24 @@
 var name = 'sidebar';
 
+var center = function(el) {
+  const menu = el.getElementsByClassName(name + '__menu')[0];
+  const active = menu.getElementsByTagName('span')[0];
+  if (!active) return;
+  const rect = active.getBoundingClientRect();
+  const center = menu.scrollTop + rect.top - ((window.innerHeight / 2) + (rect.height / 2));
+  menu.scrollTop = center;
+};
+
 var init = function() {
   var el = document.getElementsByClassName(name)[0];
 
   if (!el) return;
 
-  el.getElementsByClassName('sidebar__toggle')[0]
+  el.getElementsByClassName(name + '__toggle')[0]
     .addEventListener('click', function(e) {
       e.preventDefault();
-      el.classList.toggle('sidebar--open');
+      el.classList.toggle(name + '--open');
+      center(el);
     });
 };
 
