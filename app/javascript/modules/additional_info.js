331ee3b2febf7d3c0c14530eb473895274c8c509
dzucconi <mail@damonzucconi.com>
2017-04-27 00:15:55 -0400

    Sidebar (ibid)

---
+++ app/javascript/modules/additional_info.js
@@ -0,0 +1,15 @@
+const name = 'AdditionalInfo';
+
+const init = () => {
+  const el = document.getElementsByClassName(name)[0];
+
+  if (!el) return;
+
+  el.getElementsByClassName(`${name}__toggle`)[0]
+    .addEventListener('click', e => {
+      e.preventDefault();
+      el.classList.toggle(`${name}--open`);
+    });
+};
+
+export default init;
