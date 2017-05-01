eaa5378902ceadb17308239fdf5a90d95a300580
dzucconi <mail@damonzucconi.com>
2017-05-01 00:20:25 -0400

    Allows options to be passed into nav_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-  def nav_link(title, path)
-    link_to_unless_current title, path do
-      content_tag :span, title
+  def nav_link(title, path, options = {})
+    link_to_unless_current title, path, options do
+      content_tag :span, title, options
     end
   end
 end
