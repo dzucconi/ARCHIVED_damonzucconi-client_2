90b3f37adde6547c46df9d65859c14b9488e1c81
dzucconi <mail@damonzucconi.com>
2017-06-02 09:17:32 -0400

    Re-organizes sidebar

--- app/queries/sidebar_query.rb
+++ app/queries/sidebar_query.rb
@@ -3,14 +3,10 @@ SidebarQuery = DamonZucconiAPI::Client.parse <<-'GRAPHQL'
     artworks(state: [SELECTED, PUBLISHED]) {
       title
       slug
-      year
-      updated_at(format: "%s")
     }
     exhibitions(state: [SELECTED, PUBLISHED]) {
       title
       slug
-      year
-      updated_at(format: "%s")
     }
   }
 GRAPHQL
--- app/services/sidebar.rb
+++ app/services/sidebar.rb
@@ -1,18 +1,14 @@
 module Sidebar
   class Link
-    attr_reader :title, :updated_at
+    attr_reader :title
 
     def initialize(obj, kind)
-      @title, @slug, @year, @updated_at = obj.values_at(:title, :slug, :year, :updated_at)
+      @title, @slug = obj.values_at(:title, :slug)
       @kind = kind
     end
 
     def path
       "/#{@kind.to_s.pluralize}/#{@slug}"
-    end
-
-    def score
-      -[@year, @updated_at].join('').to_i
     end
 
     def emphasis?
@@ -26,10 +22,6 @@ module Sidebar
     def initialize(artworks, exhibitions)
       @artworks = artworks.map { |artwork| Link.new(artwork, :artwork) }
       @exhibitions = exhibitions.map { |exhibition| Link.new(exhibition, :exhibition) }
-    end
-
-    def links
-      @links ||= artworks.concat(exhibitions).sort_by(&:score)
     end
   end
 end
